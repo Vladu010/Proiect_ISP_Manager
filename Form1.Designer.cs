5dcd61a2e1481736c9e6576f1f871faaf07bc681
pault05 <paul_trif03@yahoo.com>
2025-02-20 11:05:44 +0200

    Revert "Installing NuGet packages"
    This reverts commit fd3387444b57e98c57d74d689584e83e5abad0f6.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,14 @@
         {
             button1 = new Button();
             button2 = new Button();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(713, 197);
+            button1.Location = new Point(433, 256);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -46,34 +45,24 @@
             // 
             // button2
             // 
-            button2.Location = new Point(706, 60);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Location = new Point(187, 142);
             button2.Name = "button2";
-            button2.Size = new Size(82, 22);
+            button2.Size = new Size(94, 29);
             button2.TabIndex = 1;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(180, 122);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(240, 150);
-            dataGridView1.TabIndex = 2;
-            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(914, 600);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -81,6 +70,5 @@
 
         private Button button1;
         private Button button2;
-        private DataGridView dataGridView1;
     }
 }
